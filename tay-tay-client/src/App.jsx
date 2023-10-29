bbbf723fa15da1487dd3f0f2f7e5fa5d8969e30c
Marton Verhoczki <marton.verhoczki@instructure.com>
2023-10-29 22:32:25 +0100

    Add Eslint configuration for Client

--- tay-tay-client/src/App.jsx
+++ tay-tay-client/src/App.jsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 const App = () => {
   return (
     <>
       <h1>Project Tay Tay</h1>
-      <p>It's a work in progress, but now it's React!</p>
+      <p>Its a work in progress, but now its React!</p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
