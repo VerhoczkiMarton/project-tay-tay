279207ff8ac01cc9ffb73c25f759da5213baf45b
Marton Verhoczki <marton.verhoczki@instructure.com>
2023-10-30 11:40:03 +0100

    CLIENT & SERVER Lint test #1

--- tay-tay-server/src/main/java/com/martonVerhoczki/taytayserver/HealthController.java
+++ tay-tay-server/src/main/java/com/martonVerhoczki/taytayserver/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
   @GetMapping("/api/v1/health")
-  public String health() {
+  public String Health() {
     return "OK";
   }
 }
