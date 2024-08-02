17fd5220225ee70fc3a1b59637f358742459c94e
VerhoczkiMarton <verhoczki.marci@gmail.com>
2024-08-02 21:53:26 +0200

    Update api.js

--- tay-tay-client/src/api.js
+++ tay-tay-client/src/api.js
@@ -20,13 +20,7 @@ const api = {
   },
 
   createUser: async (userData) => {
-    return api.request('/create', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
+    return api.request('/create');
   },
 };
 
